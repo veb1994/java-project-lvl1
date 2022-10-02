15196ddf9934b2ed76f0b30c695c8d26eba6f667
Valerii Bugrim <bugrimvalerii@gmail.com>
2022-10-02 19:02:15 +0700

    Add greetUser method

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,13 @@
 package hexlet.code;
 
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
         System.out.println("Welcome to the Brain Games!");
+        System.out.print("May I have your name? ");
+        Scanner sc = new Scanner(System.in);
+        String userName = sc.next();
+        System.out.println(Cli.greetUser(userName));
     }
 }
---
+++ app/src/main/java/hexlet/code/Cli.java
@@ -0,0 +1,9 @@
+package hexlet.code;
+
+import java.util.Scanner;
+
+public class Cli {
+    public static String greetUser(String userName) {
+        return "Hello, " + userName + "!";
+    }
+}
